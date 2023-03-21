3a599ab6603032137d77161e6338865e1b184b0a
0x10119 <longnq.it@gmail.com>
2023-03-21 09:34:58 -0400

    change

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,27 +94,36 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
-
+    def __str__(self):
+        return f"Enrollment for user {self.user} for course {self.course}"
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
+
+
+class Question(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
+    courses = models.ManyToManyField(Course)
+
+    question_text = models.CharField(max_length=500, default="This is a sample question.")
+
+    marks = models.FloatField(default=1.0)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
-
+    def is_get_score(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
+    def __str__(self):
+        return self.question_text
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
